2c2aee0122e27c2375fa2725435e8d334dd64626
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-06-21 10:22:31 +0530

    Time: 100 ms (76.28%) | Memory: 41.5 MB (45.26%) - LeetSync

--- minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
+++ minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
@@ -1,36 +1,42 @@
-class Solution {
+class Solution
+{
 public:
-    long long minCost(vector<int>& nums, vector<int>& cost) {
+    long long minCost(vector<int> &nums, vector<int> &cost)
+    {
         int size = nums.size();
-        vector<pair<long long,long long>> arr(size);
+        vector<pair<long long, long long> > arr(size);
         long long freq = 0;
         // making a pair vector for sorting
         // also calculating the total freq for median
-        for(int i = 0; i<size; i++){
+        for (int i = 0; i < size; i++)
+        {
             arr[i].first = nums[i];
             arr[i].second = cost[i];
-            freq+=cost[i];
+            freq += cost[i];
         }
 
         sort(arr.begin(), arr.end());
-        long long index = freq/2;
-        index--;
+        long long index = freq / 2;
+        // index--;
         freq = 0;
         long long number = 0;
 
-        // finding the median element 
+        // finding the median element
         int next_number = 0;
-        for(int i = 0; i<size; i++){
-            freq+=arr[i].second;
-            if(freq>index){
+        for (int i = 0; i < size; i++)
+        {
+            freq += arr[i].second;
+            if (freq > index)
+            {
                 number = arr[i].first;
-                if(i<size-1){
-                    next_number = arr[i+1].first;
+                if (i < size - 1)
+                {
+                    next_number = arr[i + 1].first;
                 }
                 break;
             }
         }
-        cout<<"Median is: "<<number<<endl;
+        cout << "Median is: " << number << endl;
         int count = 0;
         // number--;
         long long min_cost = LLONG_MAX;
@@ -39,27 +45,29 @@ public:
         // }
 
         // while(count<3){
-            long long c = 0;
-            cout<<"Making the number to: "<<number<<endl;
-            for(int i = 0; i<size; i++){
-                long long a = (long long)abs((arr[i].first-number));
-                c+= a*(long long)arr[i].second;
-            }
-            cout<<"cost is: "<<c<<endl;
-            min_cost = min(min_cost, c);
-            count++;
-            // number++;
+        long long c = 0;
+        cout << "Making the number to: " << number << endl;
+        for (int i = 0; i < size; i++)
+        {
+            long long a = (long long)abs((arr[i].first - number));
+            c += a * (long long)arr[i].second;
+        }
+        cout << "cost is: " << c << endl;
+        min_cost = min(min_cost, c);
+        count++;
+        // number++;
         // }
 
-        // long long 
-        c = 0;
-        cout<<"Making the number to: "<<next_number<<endl;
-        for(int i = 0; i<size; i++){
-            long long a = (long long)abs((arr[i].first-next_number));
-            c+= a*(long long)arr[i].second;
-        }
-        cout<<"cost is: "<<c<<endl;
-        min_cost = min(min_cost, c);
+        // long long
+        // c = 0;
+        // cout << "Making the number to: " << next_number << endl;
+        // for (int i = 0; i < size; i++)
+        // {
+        //     long long a = (long long)abs((arr[i].first - next_number));
+        //     c += a * (long long)arr[i].second;
+        // }
+        // cout << "cost is: " << c << endl;
+        // min_cost = min(min_cost, c);
 
         return min_cost;
     }
