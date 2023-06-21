aca07e0dcb104749cf24d5023964bd9aa2aa8fc4
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-06-21 10:25:01 +0530

    Time: 99 ms (78.10%) | Memory: 41.3 MB (45.99%) - LeetSync

--- minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
+++ minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
@@ -17,58 +17,26 @@ public:
 
         sort(arr.begin(), arr.end());
         long long index = freq / 2;
-        // index--;
         freq = 0;
-        long long number = 0;
+        long long number = 0; // median number
 
         // finding the median element
-        int next_number = 0;
         for (int i = 0; i < size; i++)
         {
             freq += arr[i].second;
             if (freq > index)
             {
                 number = arr[i].first;
-                if (i < size - 1)
-                {
-                    next_number = arr[i + 1].first;
-                }
                 break;
             }
         }
-        cout << "Median is: " << number << endl;
-        int count = 0;
-        // number--;
-        long long min_cost = LLONG_MAX;
-        // for(int i = 0; i<size; i++){
-        //     cout<<arr[i].first<<" "<<arr[i].second<<endl;
-        // }
-
-        // while(count<3){
-        long long c = 0;
-        cout << "Making the number to: " << number << endl;
+        // cout << "Median is: " << number << endl;        
+        long long min_cost = 0;
         for (int i = 0; i < size; i++)
         {
             long long a = (long long)abs((arr[i].first - number));
-            c += a * (long long)arr[i].second;
+            min_cost += a * (long long)arr[i].second;
         }
-        cout << "cost is: " << c << endl;
-        min_cost = min(min_cost, c);
-        count++;
-        // number++;
-        // }
-
-        // long long
-        // c = 0;
-        // cout << "Making the number to: " << next_number << endl;
-        // for (int i = 0; i < size; i++)
-        // {
-        //     long long a = (long long)abs((arr[i].first - next_number));
-        //     c += a * (long long)arr[i].second;
-        // }
-        // cout << "cost is: " << c << endl;
-        // min_cost = min(min_cost, c);
-
         return min_cost;
     }
 };
