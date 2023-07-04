3de00e85e3314fd1a6de0e079635fa0b925a9a31
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-07-04 17:08:36 +0530

    Time: 11 ms (5.88%) | Memory: 13.6 MB (7.00%) - LeetSync

---
+++ combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void helper(int k, int n, vector<vector<int>> &ans, vector<int> comb, int index){
+        if(k == 0){
+            if(n == 0){
+                ans.push_back(comb);
+            }
+            return;
+        }
+        if(index > 9){
+            return;
+        }
+        comb.push_back(index);
+        helper(k-1, n - index,ans,comb, index+1);
+        comb.pop_back();
+        helper(k, n,ans,comb, index+1);
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        helper(k,n, ans, comb,1);
+        return ans;
+    }
+};
