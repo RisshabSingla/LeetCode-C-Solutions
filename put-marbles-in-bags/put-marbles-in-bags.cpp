10e9a23092b27a779cb1d219d5667eb0a057639e
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-07-08 11:14:07 +0530

    Time: 215 ms (53.27%) | Memory: 63.4 MB (80.07%) - LeetSync

--- put-marbles-in-bags/put-marbles-in-bags.cpp
+++ put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
-        int size = weights.size();
+        // Approach 1: Using Priority Queue
+        // int size = weights.size();
+        // priority_queue<int> max_heap;
+        // priority_queue<int, vector<int>, greater<int> > min_heap;
+        // for(int i = 0;i<size-1; i++){
+        //     max_heap.push(weights[i] + weights[i+1]);
+        //     min_heap.push(weights[i] + weights[i+1]);
+        // }
+        // int partitions = k-1;
+        // long long ans = 0;
+        // while(partitions--){
+        //     ans+=max_heap.top();
+        //     ans-=min_heap.top();
+        //     max_heap.pop();
+        //     min_heap.pop();
+        // }
+        // return ans;
 
-        priority_queue<int> max_heap;
-        priority_queue<int, vector<int>, greater<int> > min_heap;
-        for(int i = 0;i<size-1; i++){
-            max_heap.push(weights[i] + weights[i+1]);
-            min_heap.push(weights[i] + weights[i+1]);
+        // Approach 2: Using vector and sort
+
+        int size = weights.size();
+        vector<int> sum;
+        for(int i = 0; i<size-1; i++){
+            sum.push_back(weights[i]+ weights[i+1]);
         }
-        int partitions = k-1;
+        sort(sum.begin(), sum.end());
         long long ans = 0;
-        while(partitions--){
-            ans+=max_heap.top();
-            ans-=min_heap.top();
-            max_heap.pop();
-            min_heap.pop();
+        int index = 0;
+        while(index < k-1){
+            ans-= sum[index];
+            ans+= sum[size-2-index];
+            index++;
         }
         return ans;
     }
