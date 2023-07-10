3132d3cb028f802f95c4062117ea4df968ead19b
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-07-10 10:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (64.13%) - LeetSync

--- count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
+++ count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int countOrders(int n) {
-        vector<int> dp(n+1, 0);
-        dp[0] = 1;
+        // Using Dynamic Programming
+        // vector<int> dp(n+1, 0);
+        // dp[0] = 1;
+        // int mod = 1e9+7;
+        // for(int i = 1; i<n ; i++){
+        //     long long total_gaps = 2*i + 1;
+        //     long long total_ways =  
+        //     total_gaps*total_gaps - total_gaps*(total_gaps-1)/2;            
+        //     long long a = dp[i-1] * total_ways;
+        //     a = a%mod;
+        //     dp[i] = a;
+        // }
+        // return dp[n-1];
+
+
+        // Not storing the entire array
+
         int mod = 1e9+7;
+        long long prev = 1;
         for(int i = 1; i<n ; i++){
             long long total_gaps = 2*i + 1;
-            long long total_ways =  
-            total_gaps*total_gaps - total_gaps*(total_gaps-1)/2;            
-            long long a = dp[i-1] * total_ways;
-            a = a%mod;
-            dp[i] = a;
+            long long total_ways =  total_gaps*total_gaps -     total_gaps*(total_gaps-1)/2;            
+            prev= (prev * total_ways)%mod;
         }
-        return dp[n-1];
+        return prev;
+
+
     }
 };
