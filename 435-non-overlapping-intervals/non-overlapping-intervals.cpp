9f35f93e20c547949f5f4695a78ef161faf4743f
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-07-19 09:06:41 +0530

    Time: 530 ms (30.78%) | Memory: 89.7 MB (94.24%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     static bool comp(vector<int>& a, vector<int>& b){
-        // if(a[1] == b[1]){
-        //     return a[0] < b[0];
-        // }
         return a[1] < b[1];
     } 
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
@@ -12,13 +9,6 @@ public:
             return 0;
         }
         sort(intervals.begin(), intervals.end(),comp);
-        // for(auto i: intervals){
-        //     for(auto j : i){
-        //         cout<<j<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-
         int count = 0;
         int prev = intervals[0][1];
         for(int i = 1; i<size; i++){
