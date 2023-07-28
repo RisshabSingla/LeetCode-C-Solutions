84e0d3b43f0b06b08c8921c713499d72f6100eff
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-07-28 18:24:44 +0530

    Time: 218 ms (15.07%) | Memory: 7.6 MB (29.30%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public: 
+
+    int solveMemo(vector<int>&nums, int i, int j, bool choose, vector<vector<int>> &dp){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!= -1){
+            return dp[i][j];
+        }
+        int ans = 0;
+        if(choose){
+            int ans1 = nums[i] + solve(nums,i+1, j, false);
+            int ans2 = nums[j] + solve(nums,i,j-1, false);
+            ans = max(ans1, ans2);
+        }else{
+            int ans1 = -nums[i] + solve(nums, i+1, j, true);
+            int ans2 = -nums[j] + solve(nums, i, j-1, true);
+            ans = min(ans1, ans2);
+        }
+        dp[i][j] = ans;
+        return ans;
+    }
+    
     int solve(vector<int>&nums, int i, int j, bool choose){
         if(i>j){
             return 0;
@@ -18,9 +40,10 @@ public:
     }
 
     bool PredictTheWinner(vector<int>& nums) {
-        bool choose = true;
         int size = nums.size();
-        int score = solve(nums,0,size-1, choose);
+        vector<vector<int>>dp(size, vector<int>(size, -1));
+        int score = solveMemo(nums,0,size-1, true, dp);
+        // int score = solve(nums,0,size-1, true);
         if(score>=0){
             return true;
         }
