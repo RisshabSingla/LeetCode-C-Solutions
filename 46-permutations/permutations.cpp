54e18c522bc96c7d87b62446b5b83d91f52108f7
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-08-02 16:24:48 +0530

    Time: 3 ms (67.29%) | Memory: 7.9 MB (29.89%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void helper(vector<int> nums, int index, int n ,
+            vector<vector<int>> &ans ){
+        if(index >= n){
+            // cout<<"Pushing inside ans"<<endl;
+            ans.push_back(nums);
+            return;
+        }
+
+        // cout<<"Inside"<<endl;
+        for(int i = index; i<n; i++){
+            swap(nums[index], nums[i]);
+            helper(nums, index+1, n, ans);
+            swap(nums[index], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        // cout<<"Calling"<<endl;
+        helper(nums, 0, nums.size() , ans);
+        // cout<<"Returning"<<endl;
+        return ans;
+    }
+};
