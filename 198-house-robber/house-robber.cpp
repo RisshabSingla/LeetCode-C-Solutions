2d0ad751e3b8ddc99ac618962e0a734551b5dddc
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-08-04 17:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (5.12%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -20,18 +20,29 @@ public:
     int helperDP(vector<int>&nums){
         int size = nums.size();
         vector<vector<int>> dp(2, vector<int>(size, 0));
+        int prev_inc = nums[0];
+        int prev_not_inc = 0;
 
-        dp[0][0] = nums[0];
+        int next_inc = 0;
+        int next_not_inc = 0;
+        // dp[0][0] = nums[0];
         // robbing the first house
 
         for(int i = 1; i<size; i++){
             // canRob
-            dp[0][i] = max(dp[1][i-1] + nums[i], dp[0][i-1]);
-
+            // dp[0][i] = max(dp[1][i-1] + nums[i], dp[0][i-1]);
+            next_inc = max(prev_not_inc + nums[i], prev_inc);
             // cannotRob
-            dp[1][i] = max(dp[1][i-1], dp[0][i-1]);
+            // dp[1][i] = max(dp[1][i-1], dp[0][i-1]);
+
+            next_not_inc = max(prev_inc, prev_not_inc);
+
+            prev_inc = next_inc;
+            prev_not_inc = next_not_inc;
         }
-        return max(dp[0][size-1], dp[1][size-1]);
+        return max(prev_not_inc, prev_inc);
+
+        // return max(dp[0][size-1], dp[1][size-1]);
     }
 
     int rob(vector<int>& nums) {
