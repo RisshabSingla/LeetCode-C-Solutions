5d117aa06b73d8d23e853a34efc2d3b2a2578798
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-08-05 11:55:32 +0530

    Time: 2 ms (49.17%) | Memory: 6.1 MB (10.18%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -31,12 +31,29 @@ public:
             possible += left*right;
         }
         return possible;
-
     }
+
+
+    int dynamic(int n){
+        vector<int> dp(n+1, -1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++){
+            int possible = 0;
+            for(int j = 0; j<i; j++){
+                possible += dp[j]*dp[i-j-1];
+            }
+            dp[i] = possible;
+        }
+        return dp[n];
+    }
+
     int numTrees(int n) {
         // return recursive(n);
 
-        vector<int> dp(n+1, -1);
-        return memoization(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return memoization(n, dp);
+
+        return dynamic(n);
     }
 };
