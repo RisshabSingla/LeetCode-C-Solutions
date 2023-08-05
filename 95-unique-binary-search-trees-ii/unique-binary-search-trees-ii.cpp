7459c969788738ac48e81f95b19d1db91cd42915
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-08-05 12:32:50 +0530

    Time: 12 ms (91.60%) | Memory: 12.4 MB (86.56%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -11,6 +11,41 @@
  */
 class Solution {
 public:
+
+    vector<TreeNode*> helperDP(int start, int end, map<pair<int,int>, vector<TreeNode*>> &dp){
+        vector<TreeNode*> a;
+        if(start>end){
+            a.push_back(NULL);
+            return a;
+        }
+
+        if(start == end){
+            TreeNode* root = new TreeNode(start);
+            a.push_back(root);
+            return a;
+        }
+
+        if(dp.count({start,end})){
+            return dp[{start,end}];
+        }
+
+        vector<TreeNode*> ans;
+        for(int i = start; i<=end; i++){
+            vector<TreeNode*> lft = helperDP(start, i-1,dp);
+            vector<TreeNode*> rgt = helperDP(i+1, end,dp);
+            for(int a = 0; a<lft.size(); a++){
+                for(int b = 0; b<rgt.size(); b++){
+                    TreeNode* root = new TreeNode(i);
+                    root->left = lft[a];
+                    root->right = rgt[b];
+                    ans.push_back(root);
+                }
+            }
+        }
+        dp[{start,end}] = ans;
+        return ans;
+    }
+
     vector<TreeNode*> helper(int start, int end){
         vector<TreeNode*> a;
         if(start>end){
@@ -40,6 +75,9 @@ public:
         return ans;
     }
     vector<TreeNode*> generateTrees(int n) {
-        return helper(1, n);
+        // return helper(1, n);
+
+        map< pair<int,int>, vector<TreeNode*>> dp;
+        return helperDP(1,n, dp);
     }
 };
