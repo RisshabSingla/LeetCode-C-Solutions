e21b36596e320a6deb606b556aa52eb15ce28300
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-08-15 13:02:42 +0530

    Time: 35 ms (74.06%) | Memory: 18.8 MB (90.69%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,68 +1,68 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>> ans;
-        // cout<<"Intervals is: "<<endl;
-        //     for(auto j: intervals){
-        //         for(auto k: j){
-        //             cout<<k<<" ";
-        //         }
-        //         cout<<"   ";
-        //     }
-        //     cout<<endl;
+        
+        
+        // Approach 1: Checking all the intervals and comparing
+        // -> O(n^2) 
+        // sort(intervals.begin(), intervals.end());
+        // vector<vector<int>> ans;
 
-        while(true){
-            int count = 0;
-            int size = intervals.size();
-            vector<int> indexes;
-            for(int i = 1; i<size; i++){
-                if(intervals[i][0] == -1){
-                    continue;
-                }
-                if(intervals[i][0] <= intervals[i-1][1]){
-                    // merge intervals
-                    intervals[i-1][1] = max(intervals[i][1], intervals[i-1][1]);
-                    count++;
-                    intervals[i][0] = -1;
-                    intervals[i][1] = -1;
-                    indexes.push_back(i);
-                }
-            // cout<<"indexes is: ";
-            // for(auto k: indexes){
-            //     cout<<k<<" ";
-            // }
+        // while(true){
+        //     int count = 0;
+        //     int size = intervals.size();
+        //     vector<int> indexes;
+        //     for(int i = 1; i<size; i++){
+        //         if(intervals[i][0] == -1){
+        //             continue;
+        //         }
+        //         if(intervals[i][0] <= intervals[i-1][1]){
+        //             // merge intervals
+        //             intervals[i-1][1] = max(intervals[i][1], intervals[i-1][1]);
+        //             count++;
+        //             intervals[i][0] = -1;
+        //             intervals[i][1] = -1;
+        //             indexes.push_back(i);
+        //         }
+        //     }
             
-            // cout<<endl;
-            //     cout<<"Intervals Inside is: "<<endl;
-            // for(auto j: intervals){
-            //     for(auto k: j){
-            //         cout<<k<<" ";
-            //     }
-            //     cout<<"   ";
-            // }
-            // cout<<endl;
-            }
-            
-            for(int k = indexes.size()-1; k>=0; k--){
-                intervals.erase(intervals.begin() + indexes[k]);
-            }
-            // cout<<"Intervals is: "<<endl;
-            // for(auto j: intervals){
-            //     for(auto k: j){
-            //         cout<<k<<" ";
-            //     }
-            //     cout<<"   ";
-            // }
-            // cout<<endl;
-            if(count == 0){
-                break;
+        //     for(int k = indexes.size()-1; k>=0; k--){
+        //         intervals.erase(intervals.begin() + indexes[k]);
+        //     }
+        //     if(count == 0){
+        //         break;
+        //     }
+        // }
+        // return intervals;
+
+
+
+        // Approach 2: O(n) -> Checking according to the final intervals
+
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[0]);
+        // int start = intervals[0][0];
+        int end = intervals[0][1];
+        int index = 0;
+        for(int i = 1; i<intervals.size(); i++){
+            if(intervals[i][0] <= end){
+                // merging the intervals
+                ans[index][1] = max(ans[index][1], intervals[i][1]);
+                end = ans[index][1];
+            }else{
+                ans.push_back(intervals[i]);
+                end = intervals[i][1];
+                index++;
             }
         }
-        return intervals;
+        return ans;
 
-        // for(auto i: itervals){
-        // }
+
+
+
+
+
 
     }
 };
