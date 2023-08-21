e72c9910a6187f3df6b0a5843e1749214c62b777
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-08-21 09:56:30 +0530

    Time: 232 ms (14.47%) | Memory: 9.5 MB (93.58%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        
+        // rotating the string 
+        for(int i = 1; i<s.length(); i++){
+            bool broken = false;
+            for(int j = 0; j<s.length(); j++){
+                int index = i+j;
+                index = index%(s.length());
+                char rotate = s[index];
+                char original = s[j];
+                // cout<<"Value of i is: "<<i<<endl;
+                // cout<<"Value of j is: "<<j<<endl;
+                // cout<<"Value of index is: "<<index<<endl;
+                // cout<<"Value of rotate is: "<<rotate<<endl;
+                // cout<<"Value of original is: "<<original<<endl;
+                if(rotate != original){
+                    broken = true;
+                    break;
+                }
+            }
+            if(!broken){
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+};
