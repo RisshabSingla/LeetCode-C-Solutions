7f0b06b781657c22fe1abf4f28fe409ab68d5ecb
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-08-21 10:00:00 +0530

    Time: 17 ms (70.36%) | Memory: 13.4 MB (57.60%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -3,28 +3,39 @@ public:
     bool repeatedSubstringPattern(string s) {
         
         // rotating the string 
-        for(int i = 1; i<s.length(); i++){
-            bool broken = false;
-            for(int j = 0; j<s.length(); j++){
-                int index = i+j;
-                index = index%(s.length());
-                char rotate = s[index];
-                char original = s[j];
-                // cout<<"Value of i is: "<<i<<endl;
-                // cout<<"Value of j is: "<<j<<endl;
-                // cout<<"Value of index is: "<<index<<endl;
-                // cout<<"Value of rotate is: "<<rotate<<endl;
-                // cout<<"Value of original is: "<<original<<endl;
-                if(rotate != original){
-                    broken = true;
-                    break;
-                }
-            }
-            if(!broken){
-                return true;
-            }
+        // for(int i = 1; i<s.length(); i++){
+        //     bool broken = false;
+        //     for(int j = 0; j<s.length(); j++){
+        //         int index = i+j;
+        //         index = index%(s.length());
+        //         char rotate = s[index];
+        //         char original = s[j];
+        //         // cout<<"Value of i is: "<<i<<endl;
+        //         // cout<<"Value of j is: "<<j<<endl;
+        //         // cout<<"Value of index is: "<<index<<endl;
+        //         // cout<<"Value of rotate is: "<<rotate<<endl;
+        //         // cout<<"Value of original is: "<<original<<endl;
+        //         if(rotate != original){
+        //             broken = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!broken){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+
+        int n=s.size();
+        string temp=s+s;
+        temp = temp.substr(1,temp.size()-2);
+        int ans=temp.find(s);
+        if(ans==-1)
+        {
+            return false;
         }
-        return false;
+        return true;
 
 
     }
