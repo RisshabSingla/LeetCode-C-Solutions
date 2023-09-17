ed0b51cf66d3808baf7a594c13302ae104efcffd
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-09-17 11:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (66.33%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -10,15 +10,40 @@ public:
         }
         return true;
     }
-    int countSubstrings(string s) {
+
+    int expand(string &s, int left, int right, int &slen){
         int count = 0;
-        int slen = s.length();
-        for(int i = 0; i<slen;i++){
-            for(int j = i; j<slen; j++){
-                if(isPalindrome(s, i,j)){
-                    count++;
-                }
+        while(left>=0 && right<slen){
+            if(s[left]!= s[right]){
+                break;
             }
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
+    int countSubstrings(string s) {
+        // Approach 1: Brute Force: O(n^3)
+        // int slen = s.length();
+        // int count = slen;
+        // for(int i = 0; i<slen;i++){
+        //     for(int j = i+1; j<slen; j++){
+        //         if(isPalindrome(s, i,j)){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+
+        // Approach 2: Using Expanding approach
+        int slen = s.length();
+        int count = 0;
+        for(int i = 0; i<slen; i++){
+            count+= expand(s, i, i,slen);
+            count+= expand(s, i, i+1,slen);
         }
         return count;
     }
