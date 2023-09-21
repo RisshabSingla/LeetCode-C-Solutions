2382e04b426167d1cd1caeae5c107f0bb394e4af
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-09-21 14:17:57 +0530

    Time: 10 ms (72.08%) | Memory: 13.9 MB (6.64%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,58 +4,58 @@ public:
         
         // Approach 1: Using vectors
 
-        // vector<pair<int,int>> indexes;
-        // int rows = matrix.size();
-        // int cols = matrix[0].size();
-        
-        // for(int i = 0; i<rows; i++){
-        //     for(int j = 0; j<cols; j++){
-        //         if(matrix[i][j] == 0){
-        //             indexes.push_back({i,j});
-        //         }
-        //     }
-        // }
-        
-        // if(indexes.size() == 0){
-        //     return;
-        // }
-        // for(int i = 0; i<indexes.size(); i++){
-        //     int x = indexes[i].first;
-        //     int y = indexes[i].second;
-        //     for(int c = 0; c<cols; c++){
-        //         matrix[x][c] = 0;
-        //     }
-        //     for(int r = 0; r<rows; r++){
-        //         matrix[r][y] = 0;
-        //     }
-        // }
-
-
-        // Approach 2: Using maps
+        vector<pair<int,int>> indexes;
         int rows = matrix.size();
         int cols = matrix[0].size();
-        unordered_set<int> row;
-        unordered_set<int> column;
+        
         for(int i = 0; i<rows; i++){
             for(int j = 0; j<cols; j++){
                 if(matrix[i][j] == 0){
-                    row.insert(i);
-                    column.insert(j);
+                    indexes.push_back({i,j});
                 }
             }
         }
-
-        for(auto i: row){
-            for(int j = 0; j<cols; j++){
-                matrix[i][j] = 0;
+        
+        if(indexes.size() == 0){
+            return;
+        }
+        for(int i = 0; i<indexes.size(); i++){
+            int x = indexes[i].first;
+            int y = indexes[i].second;
+            for(int c = 0; c<cols; c++){
+                matrix[x][c] = 0;
+            }
+            for(int r = 0; r<rows; r++){
+                matrix[r][y] = 0;
             }
         }
 
-        for(auto j: column){
-            for(int i = 0; i<rows; i++){
-                matrix[i][j] = 0;
-            }
-        }
+
+        // Approach 2: Using maps
+        // int rows = matrix.size();
+        // int cols = matrix[0].size();
+        // unordered_set<int> row;
+        // unordered_set<int> column;
+        // for(int i = 0; i<rows; i++){
+        //     for(int j = 0; j<cols; j++){
+        //         if(matrix[i][j] == 0){
+        //             row.insert(i);
+        //             column.insert(j);
+        //         }
+        //     }
+        // }
+
+        // for(auto i: row){
+        //     for(int j = 0; j<cols; j++){
+        //         matrix[i][j] = 0;
+        //     }
+        // }
+
+        // for(auto j: column){
+        //     for(int i = 0; i<rows; i++){
+        //         matrix[i][j] = 0;
+        //     }
+        // }
 
 
 
