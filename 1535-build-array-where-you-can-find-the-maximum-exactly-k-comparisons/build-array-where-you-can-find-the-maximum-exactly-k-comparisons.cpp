6ea19c99d47c120a18e88ee14426762f44e23a21
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-10-07 11:14:23 +0530

    Time: 67 ms (63.59%) | Memory: 7.3 MB (67.74%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+
     const int mod = 1000000007;
     int recursive(int n, int m, int k, int max_elem){
         if(n == 0){
@@ -25,7 +26,37 @@ public:
     }
 
 
-    int memo(int n, int m, int k, int max_elem, vector<vector<vector<int>>>&dp){
+    // int memo(int n, int m, int k, int max_elem, vector<vector<vector<int>>>&dp){
+    //     if(n == 0){
+    //         if(k == 0){
+    //             return 1;
+    //         }
+    //         return 0;
+    //     }
+    //     if(k<0){
+    //         return 0;
+    //     }
+
+    //     if(dp[k][n][max_elem] != -1){
+    //         return dp[k][n][max_elem];
+    //     }
+
+    //     int possible = 0;
+    //     for(int i = 1; i<=m; i++){
+    //         int a = 0;
+    //         if(i>max_elem){
+    //             a = memo(n-1, m, k-1, i,dp);
+    //         }else{
+    //             a = memo(n-1, m, k, max_elem,dp);
+    //         }
+    //         (possible+=a)%= mod;
+    //     }
+    //     return dp[k][n][max_elem] = possible;
+    // }
+
+
+    int dp[52][52][102];
+    int memo2(int n, int m, int k, int max_elem){
         if(n == 0){
             if(k == 0){
                 return 1;
@@ -39,14 +70,13 @@ public:
         if(dp[k][n][max_elem] != -1){
             return dp[k][n][max_elem];
         }
-
         int possible = 0;
         for(int i = 1; i<=m; i++){
             int a = 0;
             if(i>max_elem){
-                a = memo(n-1, m, k-1, i,dp);
+                a = memo2(n-1, m, k-1, i);
             }else{
-                a = memo(n-1, m, k, max_elem,dp);
+                a = memo2(n-1, m, k, max_elem);
             }
             (possible+=a)%= mod;
         }
@@ -62,10 +92,13 @@ public:
             return 0;
         }
 
+        memset(dp, -1, sizeof(dp));
+        return memo2(n,m, k, 0);
+
         // return recursive(n,m,k, -1);
 
-        vector<vector<vector<int>>> dp(51, vector<vector<int>>(51, vector<int>(101, -1)));
-        return memo(n,m, k,0, dp);
+        // vector<vector<vector<int>>> dp(51, vector<vector<int>>(51, vector<int>(101, -1)));
+        // return memo(n,m, k,0, dp);
 
         // return 0;
     }
