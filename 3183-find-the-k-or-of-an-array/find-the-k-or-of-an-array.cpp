3333fd920d50dce09ba179a6409becf49060a4be
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-10-29 20:03:06 +0530

    Time: 23 ms (16.67%) | Memory: 25.9 MB (33.33%) - LeetSync

--- 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.cpp
+++ 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.cpp
@@ -3,7 +3,7 @@ public:
     int findKOr(vector<int>& nums, int k) {
         int ans = 0;
         int n = nums.size();
-        for(int i = 0; i<32; i++){
+        for(int i = 0; i<31; i++){
             int count = 0;
             for(int j = 0; j<n; j++){
                 if((nums[j]>>i) & 1){
