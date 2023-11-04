740b6d9d8a1737f9636d49b4ecfd96351bde7db4
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2023-11-04 14:47:13 +0530

    Time: 15 ms (81.03%) | Memory: 23.2 MB (14.66%) - LeetSync

---
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int lsize = left.size();
+        int rsize = right.size();
+        int ans = 0;
+        for(int i = 0; i<lsize; i++){
+            ans = max(ans, left[i]);
+        }
+        for(int i = 0; i<rsize; i++){
+            ans = max(ans, n-right[i]);
+        }
+        return ans;
+    }
+};
