b37273ad7075a86c15237f6d90d2b43dac8a335f
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-01-09 15:40:15 +0530

    Time: 70 ms (48.89%) | Memory: 46.7 MB (46.37%) - LeetSync

---
+++ 3202-high-access-employees/high-access-employees.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    static bool comp(vector<string> &a, vector<string>&b ){
+        if(a[0] != b[0]){
+            return a[0] < b[0];
+        }
+        return a[1] < b[1];
+    }
+
+    int findDiff(string &str1, string &str2){
+        return (stoi(str2)- stoi(str1));
+    }
+    vector<string> findHighAccessEmployees(vector<vector<string>>& arr) {
+        sort(arr.begin(), arr.end(), comp);
+        unordered_map<string, vector<string>> timing;
+        for(auto &i: arr){
+            timing[i[0]].push_back(i[1]);
+        }
+        vector<string> ans;
+        for(auto  &i: timing){
+            if(i.second.size() < 3){
+                continue;
+            }
+            // cout<<i.first<<" ";
+            for(int j = 0; j<i.second.size()-2; j++){
+                int diff = findDiff(i.second[j], i.second[j+2]);
+                // cout<<diff<<"\n";
+                if(diff< 100){
+                    ans.push_back(i.first);
+                    break;
+                }
+            }
+            // for(auto &j: i.second){
+            //     cout<<j<<" ";
+            // }
+            // cout<<"\n";
+        }
+        return ans;
+    }
+};
