9b17a59be412f061f2ef73a1079369447a97e8b5
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-01-10 20:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (86.77%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -4,20 +4,21 @@ public:
         int bulls = 0;
         int cows = 0;
         int len = secret.length();
-        unordered_map<char, int> freq;
+        // unordered_map<char, int> freq;
+        vector<int> freq(10,0);
         for (int i = 0; i < len ; i++) {
             if (secret[i] == guess[i]) {
                 bulls++;
                 secret[i] = 'X';
                 guess[i] = 'X';
             } else {
-                freq[secret[i]]++;
+                freq[secret[i]-'0']++;
             }
-        }
+        }   
 
         for (int i = 0; i < len; i++) {
-            if (guess[i] != 'X' && freq.count(guess[i]) && freq[guess[i]] > 0) {
-                freq[guess[i]]--;
+            if (guess[i] != 'X' && freq[guess[i]-'0'] > 0) {
+                freq[guess[i]-'0']--;
                 cows++;
             }
         }
