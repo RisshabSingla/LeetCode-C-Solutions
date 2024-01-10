c82b6854424911170f47fc6ac2046198317f369b
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-01-10 13:09:48 +0530

    Time: 85 ms (63.47%) | Memory: 64 MB (19.95%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -9,13 +9,21 @@ public:
         //     }
         // }
         // return 0;
-        unordered_map<int,int> freq;
-        int size = nums.size();
-        for(int i = 0; i<size; i++){
-            if(freq.count(nums[i]) > 0 ){
+        // unordered_map<int,int> freq;
+        // int size = nums.size();
+        // for(int i = 0; i<size; i++){
+        //     if(freq.count(nums[i]) > 0 ){
+        //         return nums[i];
+        //     }
+        //     freq[nums[i]]++;
+        // }
+        int n = nums.size();
+        vector<int> arr(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            if(arr[nums[i]]){
                 return nums[i];
             }
-            freq[nums[i]]++;
+            arr[nums[i]] = 1;
         }
         return 0;
     }
