d6537590329d01cd3744200731170c5f5257d522
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-01-10 16:11:12 +0530

    Time: 18 ms (75.90%) | Memory: 19 MB (87.42%) - LeetSync

--- 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.cpp
+++ 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums, int start, int end, int n){
-        vector<int> copy;
-        
-        
-        for(int i = 0; i<start; i++){
-            copy.push_back(nums[i]);
-        }
-        for(int i = end+1; i<n; i++){
-            copy.push_back(nums[i]);
-        }
-        for(int i = 1; i<copy.size(); i++){
-            if(copy[i-1] >= copy[i]){
+    bool isValid(vector<int>& nums, int start, int end, int n) {
+        int prev = 0;
+        for (int i = 0; i < n; i++) {
+            if (i >= start && i <= end) {
+                continue;
+            }
+            if (nums[i] > prev) {
+                prev = nums[i];
+            } else {
                 return false;
             }
         }
@@ -20,9 +17,9 @@ public:
     int incremovableSubarrayCount(vector<int>& nums) {
         int n = nums.size();
         int count = 0;
-        for(int i = 0; i<n; i++){
-            for(int j = i; j<n; j++){
-                if(isValid(nums, i,j,n)){
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (isValid(nums, i, j, n)) {
                     count++;
                 }
             }
