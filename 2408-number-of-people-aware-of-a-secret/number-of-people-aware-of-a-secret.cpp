1dd9b1a6a0e0830af01e8c226967f9e7de30610b
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-01-12 20:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (48.72%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        vector<vector<long long>> people = {{delay+1, forget+1,1}};
-        long long time =1;
-        long long mod = 1e9+7;
-        while(time<= n){
+        // vector<vector<long long>> people = {{delay+1, forget+1,1}};
+        // long long time =1;
+        // long long mod = 1e9+7;
+        // while(time<= n){
             
-            while(!people.empty() && people[0][1] == time){
-                people.erase(people.begin());
-            }
-            long long count = 0;
-            for(auto &i: people){
-                if(i[0] <= time){
-                    count+= i[2];
-                }
-                count%= mod;
-            }
-            people.push_back({time+delay, time+forget, count});
-            time++;
-        }
-        long long count = 0;
-        for(auto &i: people){
-            count+= i[2];
-            count%= mod;
-        }
-        return count;
+        //     while(!people.empty() && people[0][1] == time){
+        //         people.erase(people.begin());
+        //     }
+        //     long long count = 0;
+        //     for(auto &i: people){
+        //         if(i[0] <= time){
+        //             count+= i[2];
+        //         }
+        //         count%= mod;
+        //     }
+        //     people.push_back({time+delay, time+forget, count});
+        //     time++;
+        // }
+        // long long count = 0;
+        // for(auto &i: people){
+        //     count+= i[2];
+        //     count%= mod;
+        // }
+        // return count;
         
+        vector<long long> dp(n+1, 0);
+        dp[1] = 1;
+        long long mod = 1e9+7;
+        long long peopleKnow = 0;
+        for(int i = 2; i<=n; i++){
+            if(i-delay >= 0){
+                peopleKnow+= dp[i-delay];
+                peopleKnow%= mod;
+            }
+            if(i-forget >= 0){
+                peopleKnow-= dp[i-forget];
+                peopleKnow+= mod;
+                peopleKnow%= mod;
+            }
+            dp[i] = peopleKnow;
+        }
+        peopleKnow = 0;
+        for(int i=n-forget+1;i<=n;i++){
+            peopleKnow = (peopleKnow + dp[i]) % mod;
+        }
+
+        return peopleKnow;
     }
 };
