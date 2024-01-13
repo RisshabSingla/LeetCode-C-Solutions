887cd471eecb5174028c42b0e9ba1b9150e5f895
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-01-13 16:17:45 +0530

    Time: 65 ms (80.62%) | Memory: 24.1 MB (73.79%) - LeetSync

--- 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int mod = 1e9+7;
     int dp[4001][1001];
-    int tabulation(int curr, int end , int k ){
+    int memo(int curr, int end , int k ){
         if(k == 0){
             if(curr == end){
                 return 1;
@@ -12,26 +12,12 @@ public:
         if(dp[curr+1000][k]!= -1){
             return dp[curr+1000][k];
         }
-        int forward = tabulation(curr+1, end, k-1);
-        int back = tabulation(curr-1, end, k-1);
+        int forward = memo(curr+1, end, k-1);
+        int back = memo(curr-1, end, k-1);
         dp[curr+1000][k] = (forward%mod + back%mod)%mod;
         return dp[curr+1000][k];
     }
-    // int memo(int curr, int end, int k){
-    //     if(k == 0){
-    //         if(curr == end){
-    //             return 1;
-    //         }
-    //         return 0;
-    //     }
-    //     if(dp.count({curr, k})){
-    //         return dp[{curr,k}];
-    //     }
-    //     int forward = memo(curr+1, end, k-1);
-    //     int back = memo(curr-1, end, k-1);
-    //     dp[{curr,k}] = (forward%mod + back%mod)%mod;
-    //     return dp[{curr,k}];
-    // }
+    
     // int recursive(int curr, int end, int k){
     //     if(k == 0){
     //         if(curr == end){
@@ -45,6 +31,6 @@ public:
     // }
     int numberOfWays(int startPos, int endPos, int k) {
         memset(dp,-1 ,sizeof(dp));
-        return tabulation(startPos, endPos, k);
+        return memo(startPos, endPos, k);
     }
 };
