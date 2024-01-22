7b248d008a7bd1cba963ba8945812b6442f8d3da
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-01-22 09:47:14 +0530

    Time: 313 ms (89.89%) | Memory: 198.5 MB (82.96%) - LeetSync

--- 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.cpp
+++ 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.cpp
@@ -3,10 +3,10 @@ public:
     int countDistinct(vector<int>& nums, int k, int p) {
         int count = 0;
         int n = nums.size();
-        set<vector<int>> done;
+        unordered_set<string> done;
         for(int i = 0; i<n; i++){
             int c = 0;
-            vector<int> arr;
+            string arr;
             for(int j = i; j<n; j++){
                 if(nums[j] % p == 0){
                     c++;
@@ -14,7 +14,8 @@ public:
                 if(c > k){
                     break;
                 }
-                arr.push_back(nums[j]);
+                arr.push_back('-');
+                arr+= to_string(nums[j]);
                 if(done.count(arr) == 0){
                     done.insert(arr);
                     count++;
