e5fefd8cc1abeeea7af7e1e8cda96f33bc381b0b
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-02-21 09:47:41 +0530

    Time: 7 ms (64.93%) | Memory: 8.9 MB (8.30%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -7,16 +7,35 @@ public:
         if(left == right){
             return left;
         }
-        int val = pow(2, (int)(log2(right)));
-        int curr = left;
-        // cout<<"val is: "<<val<<"\n";
-        for(long long i = max(val, left); i<=right; i++){
-            curr = curr & i;
-            // cout<<"Curr is: "<<curr<<"\n";
-            if(curr == 0){
+
+        int common = 0;
+        int ans = 0;
+        for(int i = 31; i>=0; i--){
+            if(left>>i == right>>i){
+                common++;
+                if(left>>i & 1 == 1){
+                    // int a = (1<<i);
+                    // cout<<"A is: "<<a<<"\n";
+                    // ans += a;
+                    ans += (1<<i);
+                }
+            }else{
                 break;
             }
         }
-        return curr;
+        return ans;
+
+
+        // int val = pow(2, (int)(log2(right)));
+        // int curr = left;
+        // // cout<<"val is: "<<val<<"\n";
+        // for(long long i = max(val, left); i<=right; i++){
+        //     curr = curr & i;
+        //     // cout<<"Curr is: "<<curr<<"\n";
+        //     if(curr == 0){
+        //         break;
+        //     }
+        // }
+        // return curr;
     }
 };
