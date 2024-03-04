c8fdf422fbcb0ad308a29e7c997f3c512df13cbb
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-03-04 12:06:00 +0530

    Time: 613 ms (43.39%) | Memory: 8.4 MB (50.32%) - LeetSync

--- 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isPalindrome(string str1){
+    bool isPalindrome(string &str1){
         int front = 0;
         int back = str1.length()-1;
         while(front < back){
@@ -12,21 +12,26 @@ public:
         }
         return true;
     }
-    int calcProducts(string&str, string str1, string str2, int index, int&slen){
+    int calcProducts(string&str, string &str1, string &str2, int index, int&slen){
         if(index == slen){
             if(isPalindrome(str1) && isPalindrome(str2)){
                 return str1.length() * str2.length();
             }
             return 0;
         }
-        int a = calcProducts(str, str1+str[index], str2, index+1, slen);
-        int b = calcProducts(str, str1, str2+str[index], index+1, slen);
+        str1.push_back(str[index]);
+        int a = calcProducts(str, str1, str2, index+1, slen);
+        str1.pop_back();
+        str2.push_back(str[index]);
+        int b = calcProducts(str, str1, str2, index+1, slen);
+        str2.pop_back();
         int c = calcProducts(str, str1, str2, index+1, slen);
         return max({a,b,c});
     }
     int maxProduct(string s) {
         int slen = s.length();
-        int prod = calcProducts(s, "","", 0, slen);
-        return prod;
+        string str1 = "";
+        string str2 = "";
+        return calcProducts(s, str1,str2, 0, slen);
     }
 };
