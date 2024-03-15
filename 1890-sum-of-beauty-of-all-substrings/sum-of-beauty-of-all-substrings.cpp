01cee34a5eb269c8979b7b769b4815f73d65a9d5
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-03-15 19:35:47 +0530

    Time: 149 ms (48.59%) | Memory: 10.5 MB (73.44%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -16,12 +16,34 @@ public:
         }
         return maxi-mini;
     }
+
+    int calcBeauty(vector<int>&arr1, vector<int>&arr2){
+        int mini = INT_MAX;
+        int maxi = 0;
+        for(int i = 0; i<26; i++){
+            int count = arr2[i]-arr1[i];
+            if(count == 0){
+                continue;
+            }
+            mini = min(mini, count);
+            maxi = max(maxi, count);
+        }
+        return maxi-mini;
+    }
     int beautySum(string &s) {
         int sum = 0;
         int slen = s.length();
+        vector<vector<int>> prefix(slen+1);
+        vector<int> curr(26,0);
+        prefix[0] = curr;
         for(int i = 0; i<slen; i++){
-            for(int j = i+1; j<slen; j++){
-                sum+= calcBeauty(s, i,j);
+            curr[s[i]-'a']++;
+            prefix[i+1] = curr;
+        }
+
+        for(int i = 1; i<=slen; i++){
+            for(int j = i+1; j<=slen; j++){
+                sum+= calcBeauty(prefix[i-1], prefix[j]);
             }
         }
         return sum;
