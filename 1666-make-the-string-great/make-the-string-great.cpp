88a9a33139b135ab08c33f308513df7b7f64eebb
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-04-05 22:16:43 +0530

    Time: 3 ms (48.39%) | Memory: 8.6 MB (20.58%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     string makeGood(string& str) {
-        int slen = str.length();
-        // cout<<str<<"\n";
-        while(true){
-            bool change = false;
-            for(int i = slen-1; i > 0; i--){
-                if(str[i] == str[i-1] + 32 || str[i] + 32 == str[i-1]){
-                    str.erase(str.begin() + i - 1);
-                    str.erase(str.begin() + i - 1);
-                    slen-=2;
-                    change = true;
-                }
-            }
-            // cout<<str<<"\n";
-            if(!change){
-                break;
+        // int slen = str.length();
+        // while(true){
+        //     bool change = false;
+        //     for(int i = slen-1; i > 0; i--){
+        //         if(str[i] == str[i-1] + 32 || str[i] + 32 == str[i-1]){
+        //             str.erase(str.begin() + i - 1);
+        //             str.erase(str.begin() + i - 1);
+        //             slen-=2;
+        //             change = true;
+        //         }
+        //     }
+        //     if(!change){
+        //         break;
+        //     }
+        // }
+        // return str;
+
+        stack<char> stk;
+        
+        for (char& c : str) {
+            if (!stk.empty() && abs(c - stk.top()) == 32) {
+                stk.pop();
+            } else {
+                stk.push(c);
             }
         }
-        return str;
+        
+        string result = "";
+        while (!stk.empty()) {
+            result = stk.top() + result;
+            stk.pop();
+        }
+        
+        return result;
+
     }
 };
