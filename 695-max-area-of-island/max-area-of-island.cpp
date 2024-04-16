19a2657613e0622a4a432306a77013c590a2746a
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-04-16 12:44:35 +0530

    Time: 21 ms (19.78%) | Memory: 30.5 MB (20.42%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int dir_x[4] = {-1, 0, 1, 0};
     int dir_y[4] = {0, 1, 0, -1};
-    int findArea(vector<vector<int>>& arr, int x, int y, int n, int m,
-                 vector<vector<bool>>& visited) {
+    int findArea(vector<vector<int>>& arr, int x, int y, int n, int m) {
         int count = 0;
+        arr[x][y] = INT_MAX;
         queue<pair<int, int>> q;
         q.push({x, y});
         while (!q.empty()) {
@@ -14,10 +14,9 @@ public:
             for (int i = 0; i < 4; i++) {
                 int new_x = front.first + dir_x[i];
                 int new_y = front.second + dir_y[i];
-                if (new_x >= 0 && new_x < n && new_y >= 0 && new_y < m &&
-                    !visited[new_x][new_y] && arr[new_x][new_y]) {
-                    visited[new_x][new_y] = true;
+                if (new_x >= 0 && new_x < n && new_y >= 0 && new_y < m && arr[new_x][new_y] == 1) {
                     q.push({new_x, new_y});
+                    arr[new_x][new_y] = INT_MAX;
                 }
             }
         }
@@ -27,12 +26,10 @@ public:
         int ans = 0;
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && !visited[i][j]) {
-                    visited[i][j] = true;
-                    int count = findArea(grid, i, j, n, m, visited);
+                if (grid[i][j] == 1) {
+                    int count = findArea(grid, i, j, n, m);
                     ans = max(ans, count);
                 }
             }
