bc45f147a2936744f36f653a96cfe4758899db55
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-04-19 22:56:15 +0530

    Time: 451 ms (52.88%) | Memory: 257.3 MB (5.06%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int dirX[4] = {0, 0, 1, -1};
+    int dirY[4] = {1, -1, 0, 0};
+    bool bfs(vector<vector<char>>& board, vector<vector<int>>& visited,char ch,  int i, int j, int n, int m){
+        queue<vector<int>> q;
+        q.push({i,j, i,j});
+        while(!q.empty()){
+            vector<int> p = q.front();
+            q.pop();
+            // cout<<"x: "<<p[0]<<" y: "<<p[1]<<"\n";
+            for (int k = 0; k < 4; k++) {
+                int x = p[0] + dirX[k];
+                int y = p[1] + dirY[k];
+                if(x>=0 && x<n && y>=0 && y<m && (x != p[2] || y != p[3])){
+                    // cout<<"Valid point x: "<<x<<" y: "<<y<<"\n";
+                    if(visited[x][y] && board[x][y] == ch){
+                        return true;
+                    }
+                    if(board[x][y] == ch){
+                        visited[x][y] = true;
+                        q.push({x,y, p[0], p[1]});
+                    }
+                }
+            }
+        }
+        // cout<<"Returning false\n";
+        return false;
+    }
+    bool containsCycle(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        // 1 -> right, 2 -> bottom, 3 -> left, 4 -> up
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(!visited[i][j] && bfs(grid, visited, grid[i][j], i, j, n, m)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
