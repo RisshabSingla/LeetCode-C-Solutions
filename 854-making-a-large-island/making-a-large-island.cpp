0ee0d55a58530c7fa37f055f8b9c07b74f2eaee4
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-04-22 15:00:32 +0530

    Time: 454 ms (30.94%) | Memory: 190.2 MB (23.43%) - LeetSync

---
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -0,0 +1,116 @@
+class Solution {
+public:
+    int dirX[4] = {0, 0, 1, -1};
+    int dirY[4] = {1, -1, 0, 0};
+    int findArea(vector<vector<int>>& grid, int i, int j, int n, int m) {
+        int area = 0;
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        while (!q.empty()) {
+            pair<int, int> p = q.front();
+            q.pop();
+            area++;
+            for (int k = 0; k < 4; k++) {
+                int x = p.first + dirX[k];
+                int y = p.second + dirY[k];
+                if (x >= 0 && x < n && y >= 0 && y < m && grid[x][y] &&
+                    !visited[x][y]) {
+                    visited[x][y] = true;
+                    q.push({x, y});
+                }
+            }
+        }
+        return area;
+    }
+    int dfs(vector<vector<int>>& grid, vector<vector<int>>& parent, int i,
+            int j, int n, int m, int parentIndex) {
+        if (i < 0 || i >= n || j < 0 || j >= m) {
+            return 0;
+        }
+        if (grid[i][j] == 0 || parent[i][j] != -1) {
+            return 0;
+        }
+        parent[i][j] = parentIndex;
+
+        int a = dfs(grid, parent, i - 1, j, n, m, parentIndex);
+        int b = dfs(grid, parent, i + 1, j, n, m, parentIndex);
+        int c = dfs(grid, parent, i, j - 1, n, m, parentIndex);
+        int d = dfs(grid, parent, i, j + 1, n, m, parentIndex);
+        return a + b + c + d + 1;
+    }
+
+    int findArea2(vector<vector<int>>& parent, unordered_map<int,int>& parentArea, int i, int j, int n, int m){
+        int above = 0;
+        int below = 0;
+        int left = 0;
+        int right = 0;
+        unordered_set<int> parents;
+        if(i-1>=0 && !parents.count(parent[i-1][j])){
+            above = parentArea[parent[i-1][j]];
+            parents.insert(parent[i-1][j]);
+        }
+
+        if(i+1 < n && !parents.count(parent[i+1][j])){
+            below = parentArea[parent[i+1][j]];
+            parents.insert(parent[i+1][j]);
+        }
+
+        if(j+1 < m && !parents.count(parent[i][j+1])){
+            right = parentArea[parent[i][j+1]];
+            parents.insert(parent[i][j+1]);
+        }
+
+        if(j-1 >=0 && !parents.count(parent[i][j-1])){
+            left = parentArea[parent[i][j-1]];
+            parents.insert(parent[i][j-1]);
+        }
+
+        return 1 + above + below + left + right;
+    }
+
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> parent(n, vector<int>(m, -1));
+        unordered_map<int,int> parentArea;
+        int parentIndex = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && parent[i][j] == -1) {
+                    int area = dfs(grid, parent, i, j, n, m, parentIndex);
+                    parentArea[parentIndex] = area;
+                    parentIndex++;
+                }
+            }
+        }
+
+        queue<pair<int, int>> q;
+        bool onePresent = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    q.push({i, j});
+                } else {
+                    onePresent = true;
+                }
+            }
+        }
+        if (!onePresent) {
+            return 1;
+        }
+        if (q.size() == 0) {
+            return n * m;
+        }
+        int maxi = 0;
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            int area = findArea2(parent, parentArea, x, y, n, m);
+            maxi = max(area, maxi);
+        }
+        return maxi;
+    }
+};
