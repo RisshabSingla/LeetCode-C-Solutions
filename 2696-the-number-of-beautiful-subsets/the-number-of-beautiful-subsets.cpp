33873cefdc397c447f988c2dea515d47097fc7f8
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-05-23 21:04:23 +0530

    Time: 422 ms (42.56%) | Memory: 35.6 MB (96.69%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int k, unordered_map<int, int>& freq, int i) {
-        if (i == nums.size()) { 
-            return 1;
-        }
-        int result = helper(nums, k, freq, i + 1); 
-        if (!freq[nums[i] - k] && !freq[nums[i] + k]) { 
-            freq[nums[i]]++;
-            result += helper(nums, k, freq, i + 1);
-            freq[nums[i]]--;
-        }
-        return result;
-    }
     int beautifulSubsets(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        return helper(nums, k, freq, 0) - 1;
+        return countBeautifulSubsets(nums, k, 0, 0);
+    }
+
+private:
+    int countBeautifulSubsets(vector<int>& nums, int difference, int index, int mask) {
+        // Base case: Return 1 if mask is greater than 0 (non-empty subset)
+        if (index == nums.size())
+            return mask > 0 ? 1 : 0;
+
+        // Flag to check if the current subset is beautiful
+        bool isBeautiful = true;
+
+        // Check if the current number forms a beautiful pair with any previous
+        // number in the subset
+        for (int j = 0; j < index && isBeautiful; ++j){
+            isBeautiful = ((1 << j) & mask) == 0 ||
+                          abs(nums[j] - nums[index]) != difference;
+            }
+
+        // Recursively calculate beautiful subsets including and excluding the
+        // current number
+        int skip = countBeautifulSubsets(nums, difference, index + 1, mask);
+        int take;
+        if (isBeautiful) {
+            take = countBeautifulSubsets(nums, difference, index + 1,
+                                         mask + (1 << index));
+        } else {
+            take = 0;
+        }
+
+        return skip + take;
     }
 };
