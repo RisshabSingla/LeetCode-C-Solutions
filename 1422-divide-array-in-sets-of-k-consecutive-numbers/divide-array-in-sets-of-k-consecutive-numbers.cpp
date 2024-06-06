078c3e7b7161386d009ed42b7f7aa7323a84ba7f
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-06-06 10:45:10 +0530

    Time: 136 ms (63.87%) | Memory: 53.3 MB (80.79%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -1,35 +1,54 @@
 class Solution {
 public:
     bool isPossibleDivide(vector<int>& nums, int k) {
-        unordered_map<int,int> freq;
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
+        // Approach 1: Using unordered_maps
+        // unordered_map<int,int> freq;
+        // int mini = INT_MAX;
+        // int maxi = INT_MIN;
+        // for(int i: nums){
+        //     freq[i]++;
+        //     mini = min(mini, i);
+        //     maxi = max(maxi, i);
+        // }
+        // for(auto &i: freq){
+        //     cout<<i.first<<" "<<i.second<<"\n";
+        // }
+        // for(int i = mini; i<=maxi; i++){
+        //     if(!freq.count(i) || freq[i] == 0){
+        //         continue;
+        //     }
+        //     int count = freq[i];
+        //     for(int j = 0; j<k ; j++){
+        //         if(!freq.count(i+j) || freq[i+j] < count){
+        //             return false;
+        //         }
+        //         freq[i+j]-= count;
+        //     }
+        // }
+        // return true;
+
+
+        // Approach 2: Using ordered_maps
+
+        map<int,int> freq;
         for(int i: nums){
             freq[i]++;
-            mini = min(mini, i);
-            maxi = max(maxi, i);
         }
+        // for(auto &i: freq){
+        //     cout<<i.first<<" "<<i.second<<"\n";
+        // }
+
         for(auto &i: freq){
-            cout<<i.first<<" "<<i.second<<"\n";
-        }
-        for(int i = mini; i<=maxi; i++){
-            if(!freq.count(i) || freq[i] == 0){
+            if(i.second == 0){
                 continue;
             }
-            // cout<<"seq: ";
-            int count = freq[i];
+            int count = i.second;
             for(int j = 0; j<k ; j++){
-                if(!freq.count(i+j) || freq[i+j] < count){
-                    // cout<<"\ni is: "<<i<<" j: "<<j<<"\n";
+                if(!freq.count(i.first+j) || freq[i.first+j] < count){
                     return false;
                 }
-                freq[i+j]-= count;
-                // cout<<i+j<<" ";
+                freq[i.first+j]-= count;
             }
-            // cout<<"\n";
-            // for(auto &i: freq){
-            //     cout<<i.first<<" "<<i.second<<"\n";
-            // }
         }
         return true;
     }
