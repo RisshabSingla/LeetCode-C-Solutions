a14e26313e83cc5ff92a7aadf3d5dfd927dc022a
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-07-09 11:33:38 +0530

    Time: 190 ms (15.29%) | Memory: 86.5 MB (6.47%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double wait = 0;
+        double chefTime = 0;
+        for (auto customer: customers) {
+            chefTime = max(chefTime, 1.0 * customer[0]) + customer[1];
+            wait += chefTime - customer[0];
+        }
+        return 1.0 * wait / customers.size();
+    }
+};
