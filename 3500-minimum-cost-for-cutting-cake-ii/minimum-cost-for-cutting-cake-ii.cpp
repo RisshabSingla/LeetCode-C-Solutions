6787263aad64ff7be998031075f458df09e0cd9d
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-07-14 13:10:23 +0530

    Time: 318 ms (100.00%) | Memory: 233.6 MB (100.00%) - LeetSync

---
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        sort(horizontalCut.begin(), horizontalCut.end(), greater<int>());
+        sort(verticalCut.begin(), verticalCut.end(), greater<int>());
+        int index1 = 0;
+        int index2 = 0;
+        long long verticals = 1;
+        int horizontals = 1;
+        long long ans = 0;
+        while(index1 < m-1 && index2 < n-1){
+            if(horizontalCut[index1] > verticalCut[index2]){
+                ans+= (horizontalCut[index1])*(horizontals);
+                verticals++;
+                index1++;
+            }else{
+                ans+= (verticalCut[index2])*(verticals);
+                horizontals++;
+                index2++;
+            }
+        }
+
+        while(index1 < m-1){
+            ans+= (horizontalCut[index1])*(horizontals);
+            index1++;
+        }
+
+        while(index2 < n-1){
+            ans+= (verticalCut[index2])*(verticals);
+            index2++;
+        }
+
+        return ans;
+    }
+};
