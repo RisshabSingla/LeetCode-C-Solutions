50635ec7a4719edba55d07e0bd140f4697c9dc31
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-07-29 09:29:50 +0530

    Time: 2898 ms (5.06%) | Memory: 11.7 MB (51.74%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numTeams(vector<int>& arr) {
+        int n = arr.size();
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n;j++){
+                for(int k = j+1; k<n; k++){
+                    if(arr[i] > arr[j] && arr[j] > arr[k]){
+                        count++;
+                    }
+                    if(arr[i] < arr[j] && arr[j] < arr[k]){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
