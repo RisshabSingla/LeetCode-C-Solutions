da98919c13daacdda01aa01074115a48e30f8d3d
Risshab Singla <111168601+RisshabSingla@users.noreply.github.com>
2024-07-30 19:30:16 +0530

    Time: 88 ms (64.27%) | Memory: 38.3 MB (39.07%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minimumDeletions(string str) {
-        int n= str.length();
-        vector<int> dp(n+1, 0);
+        int n = str.length();
+        vector<int> dp(n + 1, 0);
         int bCount = 0;
-        for(int i= 1; i<=n; i++){
-            dp[i] =dp[i-1];
-            if(str[i-1] == 'a'){
-                dp[i] = min(dp[i]+1, bCount);
-            }else{
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1];
+            if (str[i - 1] == 'a') {
+                dp[i] = min(dp[i] + 1, bCount);
+            } else {
                 bCount++;
             }
         }
